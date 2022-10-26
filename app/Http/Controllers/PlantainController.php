0a9e6f0442838ad3edad829cdbb538cf3ea74252
Douglas Leslie <lesliedouglas23@gmail.com>
2022-10-26 13:54:50 +0100

    trash

---
+++ app/Http/Controllers/PlantainController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plantain;
+use Illuminate\Http\Request;
+
+class PlantainController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Plantain  $plantain
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Plantain $plantain)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Plantain  $plantain
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Plantain $plantain)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Plantain  $plantain
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Plantain $plantain)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Plantain  $plantain
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Plantain $plantain)
+    {
+        //
+    }
+}
